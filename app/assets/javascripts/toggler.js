7e86df1842ae603df14785fbbd377b9fb573ef16
Dan Sensecall <dan@sensecall.co.uk>
2018-10-29 11:13:13 +0000

    search results

---
+++ app/assets/javascripts/toggler.js
@@ -0,0 +1,33 @@
+var SearchAndShortlist = SearchAndShortlist || {};
+(function ( ui ) {
+
+    ui.init = function () {
+        $('.filters .toggler').on('click', function() {
+            $(this).parents('.filters').toggleClass('unfolded');
+        });
+
+
+        $('html.js #search-results-order select').change(function() {
+            $('#search-results-order').submit();
+        });
+
+        if ($('html.js').length) {
+            var width = screen.width;
+            if (width < 641) {
+                $('.editSearch').addClass('folded');
+            }
+        }
+
+        $('html.js .editSearch h2 a').on('click', function (e) {
+            var width = screen.width;
+            if (width < 641) {
+                $('.editSearch').toggleClass('folded');
+            }
+            e.preventDefault();
+        });
+
+    };
+
+    ui.init();
+
+}(SearchAndShortlist.ui = {}))
